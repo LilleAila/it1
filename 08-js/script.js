55d784b1ec2e51c8b4724668a0c8dad13981be27
LilleAila <olai.solsvik@gmail.com>
2025-10-21 21:04:36 +0200

    idk mer js

--- 08-js/script.js
+++ 08-js/script.js
@@ -7,3 +7,6 @@ console.log(test2);
 element = document.createElement("span");
 element.innerText = "lorem ipsum dolor sit amet";
 document.body.appendChild(element);
+
+document.querySelector("#abc").innerText = "123";
+document.getElementById("def").innerText = "456";
