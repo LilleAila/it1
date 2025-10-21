0d41f04d07e89e976b3b0e2a2161e8fd4012cd8c
LilleAila <olai.solsvik@gmail.com>
2025-10-21 14:27:57 +0200

    js greier

--- 08-js/script.js
+++ 08-js/script.js
@@ -1,1 +1,9 @@
 console.log("abcde");
+
+const test = "123";
+const test2 = parseInt(test);
+console.log(test2);
+
+element = document.createElement("span");
+element.innerText = "lorem ipsum dolor sit amet";
+document.body.appendChild(element);
