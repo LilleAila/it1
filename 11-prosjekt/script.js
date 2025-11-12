c577f8ef8a7dcf8e472db1a81e92297f528886c5
LilleAila <olai.solsvik@gmail.com>
2025-11-12 09:26:16 +0100

    end test and stats

--- 11-prosjekt/script.js
+++ 11-prosjekt/script.js
@@ -10,14 +10,35 @@ function getWords(length) {
   return Array.from({ length }, () => randomFrom(words));
 }
 
+function removeChildren(elem) {
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+}
+
+function floorTo(x, n) {
+  const decimals = 10 ** n;
+  return Math.floor(x * decimals) / decimals;
+}
+
 let activeTest = {
-  length: 2,
+  started: false,
+  length: 10,
   words: [],
   currentWord: 0,
 };
 
+const statsContainer = document.querySelector(".stats > tbody");
+const wordContainer = document.querySelector(".text-display");
+const input = document.querySelector("#text-input");
+
 function newTest() {
+  activeTest.started = false;
   activeTest.currentWord = 0;
+  input.disabled = false;
+
+  removeChildren(wordContainer);
+  removeChildren(statsContainer);
 
   activeTest.words = getWords(activeTest.length).map((w) => {
     return { word: w, passed: false, correct: false };
@@ -26,13 +47,9 @@ function newTest() {
   renderWords();
 }
 
+document.querySelector("#new-test").addEventListener("click", newTest);
+
 function renderWords() {
-  const wordContainer = document.querySelector(".text-display");
-
-  while (wordContainer.firstChild) {
-    wordContainer.removeChild(wordContainer.firstChild);
-  }
-
   for (let i = 0; i < activeTest.words.length; i++) {
     const w = activeTest.words[i];
     const element = document.createElement("span");
@@ -43,30 +60,98 @@ function renderWords() {
   }
 }
 
-const input = document.querySelector("#text-input");
+function compareWords(word, target) {
+  const length = Math.max(word.length, target.length);
 
-function submitWord(word) {
-  const nextWord = activeTest.words[activeTest.currentWord];
+  return Array.from({ length }, (_, i) => word[i] === target[i]).reduce(
+    ([c, i], correct) => [c + correct, i + !correct],
+    [0, 0],
+  );
+}
 
-  nextWord.correct = word == nextWord.word;
-  nextWord.passed = true;
+function submitWord(typed) {
+  const word = activeTest.words[activeTest.currentWord];
 
-  nextWord.element.classList.add("passed");
-  nextWord.element.classList.add(nextWord.correct ? "correct" : "incorrect");
-  console.log(nextWord.element);
+  word.typed = typed;
+  word.correct = word.typed === word.word;
+  word.passed = true;
+
+  [word.correctChars, word.incorrectChars] = compareWords(
+    word.typed,
+    word.word,
+  );
+
+  word.element.classList.add("passed");
+  word.element.classList.add(word.correct ? "correct" : "incorrect");
+
+  word.end = new Date();
+  word.time = word.end - word.start; // In milliseconds
 
   activeTest.currentWord += 1;
 
   if (activeTest.currentWord >= activeTest.words.length) {
-    console.log("oi du ferdig");
-    input.disabled = true;
-    // TODO: handle test done
+    endTest();
     return;
+  }
+
+  activeTest.words[activeTest.currentWord].start = new Date();
+}
+
+function formatTime(ms) {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  const milliseconds = ms % 1000;
+
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}.${String(milliseconds).padStart(2, "0")}`;
+}
+
+function endTest() {
+  input.disabled = true;
+
+  // Time is counted per word to make it easier to expand in the future.
+  // The difference is negligible compared to only comparing start and end times
+  const time = activeTest.words.reduce((a, i) => a + i.time, 0);
+  const minutes = time / 60000;
+
+  const spaces = activeTest.words.length - 1;
+  const chars =
+    activeTest.words.reduce((a, i) => a + i.typed.length, 0) + spaces;
+  const correctChars =
+    activeTest.words.reduce((a, i) => a + i.correctChars, 0) + spaces;
+
+  const rawWpm = chars / 5 / minutes; // Measure wpm with one word defined as 5 characters on average.
+  const wpm = correctChars / 5 / minutes;
+  const accuracy = (correctChars / chars) * 100;
+
+  const stats = [
+    ["WPM", `${floorTo(wpm, 1)}`],
+    ["Raw WPM", `${floorTo(rawWpm, 1)}`],
+    ["Accuracy", `${floorTo(accuracy, 1)}%`],
+    ["Time", formatTime(time)],
+  ];
+
+  for (const [k, v] of stats) {
+    const row = document.createElement("tr");
+
+    const key = document.createElement("td");
+    key.textContent = k;
+    row.appendChild(key);
+
+    const value = document.createElement("td");
+    value.textContent = v;
+    row.appendChild(value);
+
+    statsContainer.appendChild(row);
   }
 }
 
 input.addEventListener("keydown", (e) => {
-  if (e.key == " ") {
+  if (activeTest.currentWord === 0 && !activeTest.started) {
+    // Starts the test
+    activeTest.started = true;
+    activeTest.words[0].start = new Date();
+  }
+  if (e.key === " ") {
     // TODO: maybe remove the .toLowerCase() in the future to support quotes etc?
     submitWord(input.value.trim().toLowerCase());
     input.value = "";
@@ -76,5 +161,4 @@ input.addEventListener("keydown", (e) => {
 
 (() => {
   newTest();
-  console.log(activeTest);
 })();
