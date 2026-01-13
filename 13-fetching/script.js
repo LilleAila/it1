906023a5dc1bf387af6ac14451f5f5604b8d647b
LilleAila <olai.solsvik@gmail.com>
2026-01-13 15:13:31 +0100

    13-fetching: cleanup code

--- 13-fetching/script.js
+++ 13-fetching/script.js
@@ -22,117 +22,144 @@ async function submit(e) {
     `;
   } else {
     const ds = await response.json();
+    renderDefinitions(ds);
+  }
+}
+
+async function fetchData(w) {
+  const response = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${w}`,
+  );
+
+  if (response.status != 200) {
     results.innerHTML = `
-      <p class="summary">
-        Found ${ds.length} definition${ds.length > 1 ? "s" : ""}.
-      </p>
-      ${ds
-        .map(
-          (d) => `
-            <div>
-              <h1 class="title"><code>${d.word}</code></h1>
-              <ul class="definitions">
-                <li class="phonetics">
-                  <ul>
-                    ${mkList(
-                      d.phonetics
-                        .filter((p) => p.text)
-                        .map(
-                          (p) => `
-                          ${p.text ? `<pre>${p.text}</pre>` : ""} ${
-                            "audio" in p && p.audio
+      <div class="callout info">
+        <p>The requested word was not found.</p>
+      </div>
+    `;
+  } else if (response.status != 200) {
+    results.innerHTML = `
+      <div class="callout error">
+        <p>An error occured.</p>
+      </div>
+    `;
+  } else {
+    const ds = await response.json();
+    renderDefinitions(ds);
+  }
+}
+
+function renderDefinitions(ds) {
+  results.innerHTML = `
+    <p class="summary">
+      Found ${ds.length} definition${ds.length > 1 ? "s" : ""}.
+    </p>
+    ${ds
+      .map(
+        (d) => `
+          <div>
+            <h1 class="title"><code>${d.word}</code></h1>
+            <ul class="definitions">
+              <li class="phonetics">
+                <ul>
+                  ${mkList(
+                    d.phonetics
+                      .filter((p) => p.text)
+                      .map(
+                        (p) => `
+                        ${p.text ? `<pre>${p.text}</pre>` : ""} ${
+                          "audio" in p && p.audio
+                            ? `
+                            <audio controls>
+                              <source src="${p.audio}" type="audio/mpeg">
+                            </audio>
+                            `
+                            : ""
+                        } ${"sourceUrl" in p ? `<span>(<a href="${p.sourceUrl}">Source</a> - ${mkLicense(p.license)})</span>` : ""}
+                        `,
+                      ),
+                  )}
+                </ul>
+              </li>
+              ${d.meanings
+                .map(
+                  (m) => `
+                  <li>
+                    <span class="type">${capitalize(m.partOfSpeech)}</span>
+                    <ul>
+                      ${mkList(
+                        m.definitions.map(
+                          (d) => `
+                          <p>${d.definition}</p>
+                          ${
+                            d.synonyms.length > 0
                               ? `
-                              <audio controls>
-                                <source src="${p.audio}" type="audio/mpeg">
-                              </audio>
-                              `
+                            <div>
+                              <h4>Synonyms</h4>
+                              <ul>
+                                ${mkList(m.synonyms)}
+                              </ul>
+                            </div>
+                          `
                               : ""
-                          } ${"sourceUrl" in p ? `<span>(<a href="${p.sourceUrl}">Source</a> - ${mkLicense(p.license)})</span>` : ""}
+                          }
+                          ${
+                            d.antonyms.length > 0
+                              ? `
+                            <div>
+                              <h4>Antonyms</h4>
+                              <ul>
+                                ${mkList(m.antonyms)}
+                              </ul>
+                            </div>
+                          `
+                              : ""
+                          }
                           `,
                         ),
-                    )}
-                  </ul>
-                </li>
-                ${d.meanings
-                  .map(
-                    (m) => `
-                    <li>
-                      <span class="type">${capitalize(m.partOfSpeech)}</span>
-                      <ul>
-                        ${mkList(
-                          m.definitions.map(
-                            (d) => `
-                            <p>${d.definition}</p>
-                            ${
-                              d.synonyms.length > 0
-                                ? `
-                              <div>
-                                <h4>Synonyms</h4>
-                                <ul>
-                                  ${mkList(m.synonyms)}
-                                </ul>
-                              </div>
-                            `
-                                : ""
-                            }
-                            ${
-                              d.antonyms.length > 0
-                                ? `
-                              <div>
-                                <h4>Antonyms</h4>
-                                <ul>
-                                  ${mkList(m.antonyms)}
-                                </ul>
-                              </div>
-                            `
-                                : ""
-                            }
-                            `,
-                          ),
-                        )}
-                      </ul>
-                      ${
-                        m.synonyms.length > 0
-                          ? `
-                        <div>
-                          <h3>Synonyms</h3>
-                          <ul>
-                            ${mkList(m.synonyms)}
-                          </ul>
-                        </div>
-                      `
-                          : ""
-                      }
-                      ${
-                        m.antonyms.length > 0
-                          ? `
-                        <div>
-                          <h3>Antonyms</h3>
-                          <ul>
-                            ${mkList(m.antonyms)}
-                          </ul>
-                        </div>
-                      `
-                          : ""
-                      }
-                    </li>
-                  `,
-                  )
-                  .join("")}
+                      )}
+                    </ul>
+                    ${
+                      m.synonyms.length > 0
+                        ? `
+                      <div>
+                        <h3>Synonyms</h3>
+                        <ul>
+                          ${mkList(m.synonyms)}
+                        </ul>
+                      </div>
+                    `
+                        : ""
+                    }
+                    ${
+                      m.antonyms.length > 0
+                        ? `
+                      <div>
+                        <h3>Antonyms</h3>
+                        <ul>
+                          ${mkList(m.antonyms)}
+                        </ul>
+                      </div>
+                    `
+                        : ""
+                    }
+                  </li>
+                `,
+                )
+                .join("")}
+            </ul>
+            <h2>Sources:</h2>
+            <p>
+              <ul>
+                ${mkList(d.sourceUrls.map((u) => `<a href="${u}">${u}</a>`))}
               </ul>
-              <h2>Sources:</h2>
-              <p>
-                <ul>
-                  ${mkList(d.sourceUrls.map((u) => `<a href="${u}">${u}</a>`))}
-                </ul>
-                License: ${mkLicense(d.license)}
-              </p>
-            </div>
-          `,
-        )
-        .join("<hr />")}
-    `;
-  }
+              License: ${mkLicense(d.license)}
+            </p>
+          </div>
+        `,
+      )
+      .join("<hr />")}
+  `;
 }
 
 function mkList(xs) {
@@ -143,12 +170,16 @@ function mkLicense(l) {
   return `<a href="${l.url}">${l.name}</a>`;
 }
 
-form.addEventListener("submit", submit);
-
 function capitalize(str) {
   if (!str) return "";
   return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  fetchData(data.get("query"));
+});
 
 // (() => {
 //   // Debugging to automatically look up a word
